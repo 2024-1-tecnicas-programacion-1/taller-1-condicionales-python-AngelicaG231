a41799234ae5372bc2ae768d7f1364a3eea33ccd
AngelicaG231 <maria.garzonm@konradlorenz.edu.co>
2024-03-11 19:54:25 -0500

    set de tenis
    solución

--- src/set_de_tenis.py
+++ src/set_de_tenis.py
@@ -1,12 +1,27 @@
-def evaluar(num_victorias_a, num_victorias_b):
-    # TODO: Coloca aquí el código del ejercicio 1: Set de tenis
-    return ""
+def evaluar(numVictoriasA, numVictoriasB):
+    # Verificar si el resultado es inválido
+    if numVictoriasA < 0 or numVictoriasB < 0 or abs(numVictoriasA - numVictoriasB) > 2:
+        return "Resultado inválido"
 
-if __name__ == '__main__':
-    print("Los juegos ganaddor por A:", end="")
-    num_victorias_a = int(input())
-    print("Los juegos ganaddor por B:", end="")
-    num_victorias_b = int(input())
+    # Verificar término de set
+    if numVictoriasA >= 6 and numVictoriasB >= 6:
+        if abs(numVictoriasA - numVictoriasB) >= 2:
+            return "El jugador A ganó el set" if numVictoriasA > numVictoriasB else "El jugador B ganó el set"
+        else:
+            return "El set aún no ha terminado"
 
-    respuesta = evaluar(num_victorias_a, num_victorias_b)
+    # Verificar set no terminado
+    if numVictoriasA == 6 and numVictoriasB < 5:
+        return "El set aún no ha terminado"
+    if numVictoriasB == 6 and numVictoriasA < 5:
+        return "El set aún no ha terminado"
+
+    # El set ha terminado y ninguno de los jugadores tiene más de 6 juegos
+    return "El jugador A ganó el set" if numVictoriasA > numVictoriasB else "El jugador B ganó el set" if numVictoriasB > numVictoriasA else "El set está empatado"
+
+if __name__ == "__main__":
+    numVictoriasA = int(input("Los juegos ganados por A: "))
+    numVictoriasB = int(input("Los juegos ganados por B: "))
+
+    respuesta = evaluar(numVictoriasA, numVictoriasB)
     print(respuesta)
